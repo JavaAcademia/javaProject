6c2b249eff76ecd52449fd1a8f988d4bc9f7328a
Altran <Altran@LPT-FA3140>
2017-02-23 16:06:15 +0000

    v.0.0.5

--- src/net/academia/DataBase.java
+++ src/net/academia/DataBase.java
@@ -1,19 +1,44 @@
 package net.academia;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class DataBase {
+	
 	Connection c = null;
+	
 	public DataBase() {
-
 		try {
 			Class.forName("org.sqlite.JDBC");
-			c = DriverManager.getConnection("jdbc:sqlite:test.db");
+			c = DriverManager.getConnection("jdbc:sqlite:test.db","root","root");
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
-			System.exit(0);
 		}
-		System.out.println("Opened database successfully");
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("teste.txt"));
+			Statement smt = c.createStatement();
+			String sql = "select * from TABELA;";
+			ResultSet set = smt.executeQuery(sql);
+			set.next();
+			set.getString(1);
+
+			c.setAutoCommit(false);
+			
+			c.setAutoCommit(true);
+			smt.close();
+			c.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+		}
+		
+		
 	}
 }
--- src/net/academia/Office.java
+++ src/net/academia/Office.java
@@ -1,4 +1,4 @@
-package net.academia;
+ package net.academia;
 
 public class Office {
 	
