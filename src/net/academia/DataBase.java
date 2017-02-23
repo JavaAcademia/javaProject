0d995e913a33e5c9e4db48de0fc38e6cb100090a
Altran <Altran@LPT-FA3140>
2017-02-23 17:13:36 +0000

    versao.0.0.9

--- src/net/academia/DataBase.java
+++ src/net/academia/DataBase.java
@@ -1,46 +1,36 @@
 package net.academia;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 public class DataBase {
 	
 	Connection c = null;
-	
+	boolean ligacao;
 	public DataBase() {
+		ligacao = false;
 		try {
 			Class.forName("org.sqlite.JDBC");
 			c = DriverManager.getConnection("jdbc:sqlite:test.db","root","root");
+			ligacao = true;
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
-		}
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader("teste.txt"));
-			Statement smt = c.createStatement();
-			String sql = "select * from TABELA;";
-			ResultSet set = smt.executeQuery(sql);
-			set.next();
-			set.getString(1);
-
-			c.setAutoCommit(false);
-
-			int x=0;
-
-			c.setAutoCommit(true);
-			smt.close();
-			c.close();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-		}
-		
-		
+		} 
 	}
+	
+	public DataBase(String senha, String pass){
+		ligacao = false;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:test.db",senha,pass);
+			ligacao = true;
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		} 
+	}
+	
+	public boolean isOn(){
+		return ligacao;
+	}
+	
 }
